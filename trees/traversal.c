0e04a23fcce71575a19f72100910a551be235a55
Jerry Chang <therealjerrycan@gmail.com>
2016-08-22 00:15:10 +0000

    updated make file for tree traversal methods and create mock functions for preorder, inorder, postorder traversals

---
+++ trees/traversal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "tree.h"
+
+void inorder_traversal(struct BinaryTree *root)
+{
+}
+
+void preorder_traversal(struct BinaryTree *root)
+{
+}
+
+void postorder_traversal(struct BinaryTree *root)
+{
+}
--- trees/tree.h
+++ trees/tree.h
@@ -1,14 +1,21 @@
 #ifndef TREE_H
 #define TREE_H
+/* Tree Structure  */
 struct BinaryTree {
     char *value;
     struct BinaryTree *right;
     struct BinaryTree *left;
 };
 
+/* Generic Methods  */
 struct BinaryTree *create_tree(char **s, int start, int end);
-struct BinaryTree *tree_alloc(void);
 void print_tree(struct BinaryTree *root);
+/* Memory specific */
+struct BinaryTree *tree_alloc(void);
 void free_tree(struct BinaryTree *root);
+/* Helper methods */
 char *str_dup(char **s, int mid);
+
+/* Traversal methods */
+void traverse(struct BinaryTree *root);
 #endif
