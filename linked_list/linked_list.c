adf8d214d07ba84a8279335f9215dc3b6a963d28
Jerry Chang <therealjerrycan@gmail.com>
2016-09-01 05:58:36 +0000

    added more methods for the linked list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define IS_NULL(x) x == NULL
+
 struct Node {
     char *value;
     struct Node *next;
 };
 
-struct Node *create_list(void);
+struct Node *create_list(char *s);
 struct Node *node_alloc(void);
-void add_value(char *value, struct Node *list);
+void add_node(char *value, struct Node *list);
+void remove_node(char *value, struct Node *list);
+
+int equal_value(struct Node *list, char *value);
 
 int main(void)
 {
-    struct Node *numbers = create_list();
-    add_value("A", numbers);
+    struct Node *numbers = create_list("A");
+    add_node("B", numbers);
+    remove_node("B", numbers);
     return 0;
 }
 
-struct Node *create_list(void)
+struct Node *create_list(char *s)
 {
     struct Node *temp = node_alloc();
+    temp->value = s;
     temp->next = NULL;
     return temp;
 }
@@ -29,13 +36,26 @@ struct Node *node_alloc(void)
     return (struct Node *) malloc(sizeof(struct Node));
 }
 
-void add_value(char *value, struct Node *list)
+void add_node(char *value, struct Node *node)
 {
-    if(list->next == NULL) {
-        struct Node *new_node = create_list();
-        new_node->value = value;
-        list->next = new_node;
+    if(node->next == NULL) {
+        struct Node *new_node = create_list(value);
+        node->next = new_node;
         return;
     }
-    add_value(value, list);
+    add_node(value, node->next);
+}
+
+int equal_value(struct Node *node, char *value)
+{
+    return (strcmp(node->value, value) == 0);
+}
+
+void remove_node(char *value, struct Node *node)
+{
+    if(equal_value(node, value)) {
+        return;
+    }
+    remove_node(value, node->next);
+    node->next = NULL;
 }
